b315ead4f247513885daaa8d0b80319fe42ce307
Headzy <ahumayde@gmail.com>
2023-03-15 17:15:07 +0000

    renamed currentTest.sh and improved it

--- runCode.sh
+++ runCode.sh
@@ -2,13 +2,18 @@
 
 TestFolder=currentTests
 
-
+clear
+make clean
 make bin/print_parsed -B
 
 if [[ "$?" -ne 0 ]]; then
-    echo "Build failed.";
+    echo "Build Failed.";
+else
+    echo "Build Complete."
 fi
 
 bin/print_parsed < $TestFolder/in_code.txt > $TestFolder/out_code.txt 2> $TestFolder/assembly.txt
 
-# make clean
+echo "Generated Code"
+
+make clean
